903c04e644f81fc547532cc275061538add1884e
KovalevichD <kovalevich.Demi@gmail.com>
2021-10-04 17:23:11 +0300

    remove comment, update branch name

--- src/utils/validate/checkRowOneOption.js
+++ src/utils/validate/checkRowOneOption.js
@@ -1,6 +1,5 @@
 const removeBadRows = (arrayRow, setUniqueId, setImgUrl) => {
   const uniqueId = arrayRow[0];
-  // const imageUrl = arrayRow[12];
   const imageUrl = arrayRow[16];
   let isNormalRow = false;
 
